ecfafcdeade652e48aae41c46473cac0ac57f7f2
MaoroHe <maorohecq1409@gmail.com>
2024-02-13 12:07:25 +0100

    db connexion

---
+++ assets/php/connexion/connexion.php
@@ -0,0 +1,9 @@
+<?php
+    try
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'root');
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
--- assets/php/insertDB/insert.php
+++ assets/php/insertDB/insert.php
@@ -1,1 +1,9 @@
 <?php
+    include("../connexion/connexion.php");
+
+    @$name = $_GET['name'];
+    @$firstname = $_GET['firstname'];
+    @$email = $_GET['email'];
+    @$description = $_GET['description'];
+    @$file = $_GET['file'];
+
