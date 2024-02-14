0bd6a38abec4e75b19a9a0886a96f18172f3dc2e
Ugur-F <ugur.firat240@gmail.com>
2024-02-14 14:29:30 +0100

    form v2 + ajout de la page dashboard

---
+++ dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/output.css">
+</head>
+
+<body>
+
+    <!-- NAVBAR -->
+    <nav class="bg-gray-800 p-4">
+        <div class="max-w-7xl mx-auto flex justify-between items-center shadow-2xl">
+            <div>
+                <a href="#" class="text-white text-3xl font-semibold">Hackers Poulette ™</a>
+            </div>
+            <div>
+                <ul class="flex space-x-4">
+                    <li><a href="dashboard.php" class="text-white hover:text-gray-300">Panel Admin</a></li>
+                    <li><a href="index.php" class="text-white hover:text-gray-300">Accueil</a></li>
+                    <li><a href="index.php" class="text-white hover:text-gray-300">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h2 class="text-3xl font-bold text-center mb-4 my-6">Dashboard</h2>
+
+
+
+</body>
+
+</html>
